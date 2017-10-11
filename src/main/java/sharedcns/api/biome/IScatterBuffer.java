ac6d0c247070ce2be2a3fca48af8cbf5be89712a
Dan <cyberpwnn@users.noreply.github.com>
2017-10-10 21:08:39 -0400

    Scatter buffer fix

--- src/main/java/sharedcns/api/biome/IScatterBuffer.java
+++ src/main/java/sharedcns/api/biome/IScatterBuffer.java
@@ -9,6 +9,10 @@ public interface IScatterBuffer
 {
 	public int getStrength();
 	
+	public DecorationPass getDecorationPass();
+	
+	public void setDecorationPass(DecorationPass pass);
+	
 	public double getChance();
 	
 	public boolean canDecorate(World w, Random r, DecorationPass pass, Block surface, int x, int y, int z, BiomeTemperatureModifier temperatureModifier, BiomeHumidityModifier humidityModifier, BiomeTemperature temp, BiomeHumidity hum);
