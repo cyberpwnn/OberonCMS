0ab11b338035fa92254a75acf1653ea40b7b4cea
Dan <cyberpwnn@users.noreply.github.com>
2017-10-14 04:56:31 -0400

    River biome

---
+++ src/main/java/sharedcms/content/world/biome/BiomeRiver.java
@@ -0,0 +1,39 @@
+package sharedcms.content.world.biome;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenOcean;
+import sharedcms.base.AresBlockShrub;
+import sharedcms.content.Content;
+import sharedcms.content.Content.Block;
+import sharedcms.content.world.buffer.scatter.ShrubBuffer;
+import sharedcms.content.world.buffer.scatter.TreeBuffer;
+import sharedcms.content.world.buffer.scatter.TreeModel;
+import sharedcms.content.world.buffer.scatter.WaterBuffer;
+import sharedcms.content.world.buffer.surface.SolidSurfaceBuffer;
+import sharedcms.content.world.buffer.surface.SplitSurfaceBuffer;
+import sharedcms.content.world.meta.objects.MetaWorld;
+import sharedcms.util.GEN;
+import sharedcms.util.SimplexProperties;
+import sharedcns.api.biome.BiomeHumidity;
+import sharedcns.api.biome.BiomeHumidityModifier;
+import sharedcns.api.biome.BiomeTemperature;
+import sharedcns.api.biome.BiomeTemperatureModifier;
+import sharedcns.api.biome.DecorationPass;
+
+public class BiomeRiver extends LBiomeBase
+{
+	public BiomeRiver(int level)
+	{
+		super(level);
+
+		height = -0.9f;
+		variation = 0f;
+		setBiomeHumidity(BiomeHumidity.DRENCHED);
+		setBiomeTemperature(BiomeTemperature.CHILLY);
+
+		SplitSurfaceBuffer buffer = new SplitSurfaceBuffer(Blocks.air, Content.Block.PATH_SAND, Content.Block.COLD_STONE);
+
+		setSurfaceBuffer(buffer);
+	}
+}
