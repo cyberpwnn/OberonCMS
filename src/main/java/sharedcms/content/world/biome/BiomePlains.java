f33f8d1bc4196f6b3d9771419e15d2964e1d39da
Dan <cyberpwnn@users.noreply.github.com>
2017-10-16 03:23:25 -0400

    Brushes

--- src/main/java/sharedcms/content/world/biome/BiomePlains.java
+++ src/main/java/sharedcms/content/world/biome/BiomePlains.java
@@ -1,13 +1,23 @@
 package sharedcms.content.world.biome;
 
-import net.minecraft.init.Blocks;
+import java.util.Random;
+
+import net.minecraft.world.World;
 import sharedcms.base.AresBlockShrub;
 import sharedcms.content.Content;
-import sharedcms.content.Content.Block;
+import sharedcms.content.structure.brush.Brush;
+import sharedcms.content.structure.model.Model;
+import sharedcms.content.structure.model.ModelMashMode;
+import sharedcms.content.structure.model.ModelMaterial;
+import sharedcms.content.world.buffer.scatter.ModelBuffer;
 import sharedcms.content.world.buffer.scatter.ShrubBuffer;
 import sharedcms.content.world.buffer.surface.SolidSurfaceBuffer;
+import sharedcms.util.Location;
 import sharedcns.api.biome.BiomeHumidity;
+import sharedcns.api.biome.BiomeHumidityModifier;
 import sharedcns.api.biome.BiomeTemperature;
+import sharedcns.api.biome.BiomeTemperatureModifier;
+import sharedcns.api.biome.DecorationPass;
 
 public class BiomePlains extends LBiomeBase
 {
@@ -17,7 +27,7 @@ public class BiomePlains extends LBiomeBase
 		setBiomeHumidity(BiomeHumidity.DAMP);
 		setBiomeTemperature(BiomeTemperature.WARM);
 		setSurfaceBuffer(new SolidSurfaceBuffer(Content.Block.COLD_GRASS, Content.Block.COLD_DIRT, Content.Block.COLD_STONE));
-		
+
 		ShrubBuffer main = new ShrubBuffer(80);
 		main.addSoil(Content.Block.COLD_GRASS);
 		main.addBlock(Content.Block.THIN_GRASS, 20);
@@ -26,22 +36,22 @@ public class BiomePlains extends LBiomeBase
 		main.addBlock(Content.Block.SPOKED_GRASS, 20);
 		main.bindAllHumidities();
 		main.bindAllTemperatures();
-		
+
 		ShrubBuffer normal = new ShrubBuffer(50);
 		normal.addSoil(Content.Block.COLD_GRASS);
-		
-		for(AresBlockShrub i : Content.flowers()) 
+
+		for(AresBlockShrub i : Content.flowers())
 		{
 			normal.addBlock(i, i.getWeight());
 		}
-		
+
 		normal.bindAllHumidities();
 		normal.unbindHumidity(BiomeHumidity.ARID);
 		normal.unbindHumidity(BiomeHumidity.DRY);
 		normal.bindTemperature(BiomeTemperature.CHILLY);
 		normal.bindTemperature(BiomeTemperature.WARM);
 		normal.bindTemperature(BiomeTemperature.HOT);
-	
+
 		ShrubBuffer hot = new ShrubBuffer(56);
 		hot.addSoil(Content.Block.COLD_GRASS);
 		hot.addBlock(Content.Block.DEAD_BUSH, 20);
@@ -50,7 +60,7 @@ public class BiomePlains extends LBiomeBase
 		hot.bindHumidity(BiomeHumidity.DRY);
 		hot.bindTemperature(BiomeTemperature.HOT);
 		hot.bindTemperature(BiomeTemperature.SCORCHED);
-		
+
 		ShrubBuffer dry = new ShrubBuffer(56);
 		dry.addSoil(Content.Block.COLD_GRASS);
 		dry.addBlock(Content.Block.DEAD_BUSH, 20);
@@ -60,7 +70,7 @@ public class BiomePlains extends LBiomeBase
 		dry.unbindHumidity(BiomeHumidity.MOIST);
 		dry.bindTemperature(BiomeTemperature.HOT);
 		dry.bindTemperature(BiomeTemperature.WARM);
-	
+
 		addScatterBuffer(main);
 		addScatterBuffer(normal);
 		addScatterBuffer(dry);
